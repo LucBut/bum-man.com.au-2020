b9bfb101462a82516a7b259121f5688d717b6de0
John Noble <2020@johnny.net.au>
2020-03-20 16:22:51 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Container, Divider } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
-function App() {
+const BumMan = props => {
+  const { alt, src } = props;
+  console.log(alt, src);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div align="center">
+      <LazyLoadImage
+        alt={alt}
+        height={400}
+        src={src} // use normal <img> attributes as props
+        width={400}
+      />
+      <p>Lazy pink.</p>
+      <Divider />
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Container text style={{ margin: '50px 0 100px 0' }}>
+      <BumMan alt="Pink" src="/bumman/1_Classic_pink.jpg" />
+      <BumMan alt="Pink" src="/bumman/2_Running_green.jpg" />
+      <BumMan alt="Pink" src="/bumman/3_Leggy_blue.jpg" />
+      <BumMan alt="Pink" src="/bumman/4_Body hugging.jpg" />
+      <BumMan alt="Pink" src="/bumman/1_Classic_pink.jpg" />
+      <BumMan alt="Pink" src="/bumman/2_Running_green.jpg" />
+      <BumMan alt="Pink" src="/bumman/3_Leggy_blue.jpg" />
+      <BumMan alt="Pink" src="/bumman/4_Body hugging.jpg" />
+      <BumMan alt="Pink" src="/bumman/1_Classic_pink.jpg" />
+      <BumMan alt="Pink" src="/bumman/2_Running_green.jpg" />
+      <BumMan alt="Pink" src="/bumman/3_Leggy_blue.jpg" />
+      <BumMan alt="Pink" src="/bumman/4_Body hugging.jpg" />
+      <BumMan alt="Pink" src="/bumman/1_Classic_pink.jpg" />
+      <BumMan alt="Pink" src="/bumman/2_Running_green.jpg" />
+      <BumMan alt="Pink" src="/bumman/3_Leggy_blue.jpg" />
+      <BumMan alt="Pink" src="/bumman/4_Body hugging.jpg" />
+      <BumMan alt="Pink" src="/bumman/1_Classic_pink.jpg" />
+      <BumMan alt="Pink" src="/bumman/2_Running_green.jpg" />
+      <BumMan alt="Pink" src="/bumman/3_Leggy_blue.jpg" />
+      <BumMan alt="Pink" src="/bumman/4_Body hugging.jpg" />
+      <BumMan alt="Pink" src="/bumman/1_Classic_pink.jpg" />
+      <BumMan alt="Pink" src="/bumman/2_Running_green.jpg" />
+      <BumMan alt="Pink" src="/bumman/3_Leggy_blue.jpg" />
+      <BumMan alt="Pink" src="/bumman/4_Body hugging.jpg" />
+      <BumMan alt="Pink" src="/bumman/1_Classic_pink.jpg" />
+      <BumMan alt="Pink" src="/bumman/2_Running_green.jpg" />
+      <BumMan alt="Pink" src="/bumman/3_Leggy_blue.jpg" />
+      <BumMan alt="Pink" src="/bumman/4_Body hugging.jpg" />
+    </Container>
+  );
+};
 
 export default App;

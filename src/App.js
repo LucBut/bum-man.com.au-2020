35f90f0aef4cf9cc07f677fae4e4154d61131abc
John Noble <2020@johnny.net.au>
2020-03-20 17:16:37 +1100

    Layout tweaks, fix the headers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,42 @@ import 'semantic-ui-css/semantic.min.css';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import './image.css';
 
+const TopSection = () => (
+  <>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 3fr',
+        marginBottom: '50px',
+      }}
+    >
+      <div style={{ gridColumn: 1 }}>
+        <img src="/bumman/Profile image_bum crack.jpg" />
+      </div>
+      <div style={{ alignSelf: 'center', gridColumn: 2, marginLeft: '50px' }}>
+        <Header as="h1">Bum Man</Header>
+      </div>
+    </div>
+
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '4fr 1fr',
+      }}
+    >
+      <div style={{ fontSize: '1.1em', gridColumn: 1 }}>
+        Born around 1991 in Clarence, NSW, Australia.
+      </div>
+      <div style={{ gridColumn: 3, justifySelf: 'end' }}>
+        <a href="https://www.instagram.com/thebummandiaries/">
+          <LazyLoadImage src="/instagram.png" />
+        </a>
+      </div>
+    </div>
+    <Divider />
+  </>
+);
+
 const BumPost = ({ date, instagram, image, title }) => {
   return (
     <div>
@@ -25,6 +61,8 @@ const BumPost = ({ date, instagram, image, title }) => {
 const App = () => {
   return (
     <Container text style={{ margin: '50px 0 100px 0' }}>
+      <TopSection />
+
       <BumPost
         date="19th March 2020"
         instagram="https://www.instagram.com/p/B950dXClcY4/"
